88635b0cbc8e00f08ab578ca34a3450723363326
msiddharthreddy <msiddharthreddy@github.com>
2018-08-18 19:50:22 -0700

    addResourceTags

--- crawler.js
+++ crawler.js
@@ -2,6 +2,12 @@ const scraper = require('google-search-scraper');
 const doMongo = require('./mongo.js');
 
 const RESOURCES = 'resources';
+const USET_EVENTS = 'userEvents';
+
+doMongo(async(db, err) => {
+  const userEvents = await db.collection(USET_EVENTS).find().toArray();;
+  console.log('User Events', userEvent)
+});
 
 
 
@@ -13,24 +19,26 @@ let options = {
 let titles = [];
 let urls = [];
 
-scraper.search(options, function(err, url, meta) {
-  // This is called for each result
-  if(err) throw err;
-  doMongo(async(db, err) => new Promise((res, rej) => {
-    db.collection(RESOURCES).updateOne({
-      link: url
-    },
-    {$set: {
-      name: meta.title,
-      link: url
-    }}, {
-      upsert: true
-    },
-     (err, result) => {
-      if (err) console.error(err);
-      else console.log(result);
-      return res({});
-    });
-  }))
+const search(options) {
+  scraper.search(options, function(err, url, meta) {
+    // This is called for each result
+    if(err) throw err;
+    doMongo(async(db, err) => new Promise((res, rej) => {
+      db.collection(RESOURCES).updateOne({
+        link: url
+      },
+      {$set: {
+        name: meta.title,
+        link: url
+      }}, {
+        upsert: true
+      },
+       (err, result) => {
+        if (err) console.error(err);
+        else console.log(result);
+        return res({});
+      });
+    }))
 
-});
+  });
+}
